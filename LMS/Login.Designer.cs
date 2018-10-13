9d241a7be8ee6f8680d659a09efa1c2d6b78dd52
Tavi66 <42556132+Tavi66@users.noreply.github.com>
2018-10-13 13:50:50 -0500

    Frontend -Added basic login UI -Added loginButton clickable event -Added username and password labels/ text fields - hidden character for password -Added second form -resized window -renamed form

---
+++ LMS/Login.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace LMS
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton = new System.Windows.Forms.Button();
+            this.passwordEntry = new System.Windows.Forms.TextBox();
+            this.usernameEntry = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginPanel = new System.Windows.Forms.Panel();
+            this.loginPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.DimGray;
+            this.loginButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.loginButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(41, 197);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(250, 56);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "LOGIN";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // passwordEntry
+            // 
+            this.passwordEntry.Location = new System.Drawing.Point(41, 143);
+            this.passwordEntry.Name = "passwordEntry";
+            this.passwordEntry.PasswordChar = '*';
+            this.passwordEntry.Size = new System.Drawing.Size(250, 22);
+            this.passwordEntry.TabIndex = 1;
+            // 
+            // usernameEntry
+            // 
+            this.usernameEntry.HideSelection = false;
+            this.usernameEntry.Location = new System.Drawing.Point(41, 79);
+            this.usernameEntry.Name = "usernameEntry";
+            this.usernameEntry.Size = new System.Drawing.Size(250, 22);
+            this.usernameEntry.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // loginPanel
+            // 
+            this.loginPanel.BackColor = System.Drawing.Color.LightGray;
+            this.loginPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loginPanel.Controls.Add(this.label2);
+            this.loginPanel.Controls.Add(this.loginButton);
+            this.loginPanel.Controls.Add(this.label1);
+            this.loginPanel.Controls.Add(this.passwordEntry);
+            this.loginPanel.Controls.Add(this.usernameEntry);
+            this.loginPanel.Location = new System.Drawing.Point(324, 124);
+            this.loginPanel.Name = "loginPanel";
+            this.loginPanel.Size = new System.Drawing.Size(344, 314);
+            this.loginPanel.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 581);
+            this.Controls.Add(this.loginPanel);
+            this.Name = "Login";
+            this.Text = "LMS";
+            this.loginPanel.ResumeLayout(false);
+            this.loginPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox passwordEntry;
+        private System.Windows.Forms.TextBox usernameEntry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel loginPanel;
+    }
+}
+
